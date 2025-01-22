9560c58d068e3e8a15a61ccce6dc94f52a5f7685
dhruvaa24 <dhruvagrawal247@gmail.com>
2025-01-22 19:44:45 +0530

    basic flow of a React project

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,12 @@
+import Chai from "./Chai.jsx"
 
 function App() {
   return (
-    <h1>Hello world! || Dhruv</h1>
+    <>
+      <Chai />
+      {/* // <h1> Chai aur rec</h1> // this will give error as according to the rule you can return only one html element. to solve this the extra tags were wrapped inside a div element or empty tag called fragment <> </> */}
+      <p>Test Para</p>
+    </>
   )
 }
 
