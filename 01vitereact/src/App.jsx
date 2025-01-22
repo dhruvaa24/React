029e525205bc4a575a954649da2878ca4d386d87
dhruvaa24 <dhruvagrawal247@gmail.com>
2025-01-23 03:40:41 +0530

    creating custom react and basics about rendering elements

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 import Chai from "./Chai.jsx"
 
 function App() {
+  const username = "Dhruv Agrawal"
+
   return (
     <>
       <Chai />
       {/* // <h1> Chai aur rec</h1> // this will give error as according to the rule you can return only one html element. to solve this the extra tags were wrapped inside a div element or empty tag called fragment <> </> */}
+      <h1>Dhruvaa24 {username}</h1>
+      {/* {username} is evaluated outcome that means you cant write whole javascript code inside {} but you ca only write the final outcomes of javascript written outside the return  */}
       <p>Test Para</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+      <div>
+        <h1>Custom App!</h1>
+      </div>
+    )
+} // The function parses the return into a reactElement just like written below.And then we can pass this function inside the render function of the Root but we can not directly pass the custom reactElement that we make as render is a functin that accepts only a function with specific arguments and then executes itself but if we pass a custom reactElement it wont work as we dont know the parameters.
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit GOOGLE'
+// }
+
+const user = " dhruvaaaaa24" // we can insert this variable in our custom reeact element and it is injected directly. for the same reason we write evaluated outcome in our return statement in the {} brackets since the return values by the end of the day have to be converted in the parse tree as below so if we would have writeen a javascript code within {} it would have given an error when they will convert in parse tree
+
+const reactElement = React.createElement(
+    'a',
+    {
+      href: 'https://www.google.com',
+      target: '_blank'
+    },
+    'Click me to visit Google',
+    user
+)//But this custom reactElement will work as we made it as per the requiremnets and syntax required by the render function 
+
+const anotherElement = (
+    <a href="https://www.google.com" target='_blank'>Visit Google</a>
+) // And this custom reactElement will work as its syntax is correct and will get rendered by the render function
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    // <MyApp />
+    reactElement
+    // anotherElement
+    //<App />
+  
 )
