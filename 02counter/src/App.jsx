096eb5719bd04e9234e9fd1afa7f98bfd0311ea8
dhruvaa24 <dhruvagrawal247@gmail.com>
2025-01-23 16:28:44 +0530

    project: increment and decrement counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 5
+
+  const addValue = () => {
+      if(counter < 20){
+          counter++;
+          setCounter(counter)
+          console.log("Added", counter)
+      } else {
+          alert('Counter value should be between 0 and 20')
+      }
+  }
+
+  const removeValue = () => {
+      if(counter > 0){
+          counter--;
+          setCounter(counter)
+          console.log("Subtracted", counter)
+      } else {
+          alert('Counter value should be between 0 and 20')
+      }
+  }
+
+  return (
+    <>
+      <h1>React Series</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick = {addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick= {removeValue}>Remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
