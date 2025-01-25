2a08a537048337c5f0e4c0851b719a7afcc7a11b
dhruvaa24 <dhruvagrawal247@gmail.com>
2025-01-25 16:52:48 +0530

    added background image

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-10 py-5 my-50 bg-gray-800 text-orange-500 ">
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-10 py-5 my-50 bg-gray-950 text-orange-500 ">
         <h1 className="text-white text-center mb-2">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
